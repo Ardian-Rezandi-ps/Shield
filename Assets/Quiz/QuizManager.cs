f692b1a09b4b02a7e01b803cbe38445116b54850
Ardian Rezandi <ardianrezandi@gmail.com>
2022-10-22 10:50:15 +0700

    Quiz skrip

--- Assets/Quiz/QuizManager.cs
+++ Assets/Quiz/QuizManager.cs
@@ -3,66 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class QuizManager : MonoBehaviour
 {
-    public string soal;
-    public string[] jawaban;
+   
     public int indexJawabanBenar;
-
-    public Text textSoal;
-    public Text[] textJawaban;
-
-    public Text textScore;
-    public static int score = 0;
-    public static int nomorSoal = 1;    
-    public static int maxSoal = 3;
+    public SoalJawab[] kumpulanSoalSkrip;
+    public TextMeshProUGUI textSoal;
+    public List<TextMeshProUGUI> textJawaban;
+    public GameObject uiPenjelasan,uiKoreksi, uiSkor;
+    public TextMeshProUGUI textScore, pointx1,pointx2,timetxskor,acctx,korektx,salahBenartx,deskPenjelasantx;
+    public Image imgPenjelasan;
+    
+    public static int score = 0,correcrt=0;
+    public static int nomorSoal = 0;    
+    //public static int maxSoal = 3;
+     public int indekBenar;
     public Image timerImg;
     public GameObject panelWin;
-    public float timerTime=0,timerMax=0;
+    public float timerTime=0,timerMax=0,decreasetime=0.1f,timeUsed=0,timecumulative=0;
+    
 
     private void Update() {
         if(Time.time >= timerTime+1){
-            timerImg.fill -= 0.1f;
+            timeUsed++;
+             timecumulative ++;
+            timerImg.fillAmount -= decreasetime;
             timerTime=Time.time;
-            
+            if(timerImg.fillAmount <=0){
+                WaktuHabis();
+            }
         }
+       
         
+    }
+    public void WaktuHabis(){
+        timerImg.fillAmount=0;
     }
     void Start()
     {
 
-        timerTime=Time.time;
-      //  textSoal.text = soal;
-       // textScore.text = "Score : " + score.ToString();
-
-        //versi 1
-        //textJawaban[0].text = jawaban[0];
-        //textJawaban[1].text = jawaban[1];
-        //textJawaban[2].text = jawaban[2];
-        //textJawaban[3].text = jawaban[3];
-
-        //versi 2
-        for(int i=0; i<=3; i++){
-        //    textJawaban[i].text = jawaban[i];
+       decreasetime= 1/timerMax;
+   
+      LoadSoal();
+    }
+    public void NextSoal(){
+        nomorSoal++;
+        textScore.text=""+score+" point.";
+        pointx1.text= ""+score+" point.";   
+          pointx2.text= ""+score+" point.";
+          timeUsed=0;
+        if(nomorSoal< kumpulanSoalSkrip.Length){
+            LoadSoal();
+        }else{
+            uiSkor.SetActive(true);
+            print("Selesai");
         }
     }
+    public void LoadSoal(){
+         timerTime=Time.time;
+        
+            timerImg.fillAmount = 1;
 
+            deskPenjelasantx.text= kumpulanSoalSkrip[nomorSoal].deskPenjelasan;
+            imgPenjelasan.sprite= kumpulanSoalSkrip[nomorSoal].gambarPenjelasan;
+        textSoal.text= kumpulanSoalSkrip[nomorSoal].soal;
+        timetxskor.text = ""+timecumulative+" detik.";
+        korektx.text= ""+correcrt+" Correct.";
+        float accurate= correcrt/kumpulanSoalSkrip.Length;
+        acctx.text= ""+accurate+" %";
+
+
+        for(int i=0;i<kumpulanSoalSkrip[nomorSoal].jawaban.Length;i++){
+            textJawaban[i].text= kumpulanSoalSkrip[nomorSoal].jawaban[i];
+          
+        }
+        indekBenar= kumpulanSoalSkrip[nomorSoal].indekBenar;
+    }
     public void ValidasiJawaban(int indexJawabanPlayer)
     {
         if(indexJawabanPlayer == indexJawabanBenar){
-            score += 10;
+            score += 100;
+            correcrt++;
+          salahBenartx.text="Jawaban Benar.";
         }
         else {
-            score -= 5;
+            score -= 0;
+             salahBenartx.text="Jawaban Salah.";
         }
 
-        if(nomorSoal == maxSoal) {
-            panelWin.SetActive(true);
-        }
-        else{
-            nomorSoal +=1;
-            SceneManager.LoadScene("Quiz" + nomorSoal);
-        }
+       
+           uiKoreksi.SetActive(true);
+      
     }
+
 }
---
+++ Assets/Quiz/SoalJawab.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoalJawab : MonoBehaviour
+{
+     public string soal,deskPenjelasan;
+    public string[] jawaban;
+    public int indekSoal, indekBenar;
+
+    public Sprite gambarPenjelasan;
+    // Start is called before the first frame update
+  
+}
